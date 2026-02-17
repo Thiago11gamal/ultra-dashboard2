8ebe4b8defb19be1d040ea1faf61edaf7f830f55
Thiago Antunes <antunest040@gmail.com>
2026-02-17 16:44:53 -0400

    jhjghjnyrftgyhugggggggg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { RotateCcw, CalendarDays, Calendar, X } from 'lucide-react';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
