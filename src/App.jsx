fd82dc35819949291be1ba82d263f87a7f8b9866
Thiago Antunes <antunest040@gmail.com>
2026-03-02 01:19:18 -0400

    yyttrdrdgghjjjnhbbb

--- src/App.jsx
+++ src/App.jsx
@@ -256,7 +256,7 @@ function MainLayout() {
         onOpenHelp={() => setShowHelpGuide(true)}
       />
 
-      <main className="px-6 lg:px-8 pt-28 pb-16 transition-all duration-300 w-full overflow-clip">
+      <main className="px-6 lg:px-8 pt-28 pb-16 transition-all duration-300 w-full overflow-x-hidden">
         <Header
           user={data.user}
           settings={data.settings}
