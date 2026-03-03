3780c60d1488fdc336069b9571faa2aac210f0d8
Thiago Antunes <antunest040@gmail.com>
2026-03-03 02:45:32 -0400

    jh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -321,7 +321,7 @@ export default function Header({
                             Foco Principal ✏️
                         </div>
                     </div>
-                    <div className="mt-6">
+                    <div className="mt-16">
                         <DateDisplay time={clockTime} />
                     </div>
                 </div>
