40ad9ea814d75b05b2018147ee20b5f432e8876c
Thiago Antunes <antunest040@gmail.com>
2026-03-07 15:29:39 -0400

    uiuutyurtyeyuyiutnbrtvevtbynubv

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -4,8 +4,5 @@
  */
 let _counter = 0;
 export const generateId = (prefix = 'id') => {
-    const timestamp = Date.now();
-    const seq = (_counter++ % 9999).toString().padStart(4, '0');
-    const random = Math.random().toString(36).substring(2, 7);
-    return `${prefix}-${timestamp}-${seq}-${random}`;
+    return `${prefix}-${crypto.randomUUID()}`;
 };
