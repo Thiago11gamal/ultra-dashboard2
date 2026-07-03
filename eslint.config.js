2da6669f95f88ab27e3f496925d62c59f52fbb11
Thiago11gamal <antunest040@gmail.com>
2026-07-02 23:50:34 -0400

    chore(lint): ignore binary and artifact scripts at project root

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'move*.js', 'move*.cjs', 'script.js']),
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
