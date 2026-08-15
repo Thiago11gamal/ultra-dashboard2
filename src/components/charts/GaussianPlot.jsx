15dcb093bcb29f3b43443ad78bea89d84994f716
Thiago11gamal <antunest040@gmail.com>
2026-08-14 21:03:56 -0400

    fvghy

--- src/components/charts/GaussianPlot.jsx
+++ src/components/charts/GaussianPlot.jsx
@@ -323,7 +323,7 @@ export const GaussianPlot = ({
 
     return (
         <div
-            className="relative w-full h-[220px] mt-28 mb-16 pb-6 cursor-crosshair group/chart"
+            className="relative w-full h-[200px] mt-20 mb-2 pb-6 cursor-crosshair group/chart"
             onMouseMove={(e) => updateHoverFromClientX(e.clientX, e.currentTarget)}
             onMouseLeave={clearHover}
             onTouchStart={(e) => {
